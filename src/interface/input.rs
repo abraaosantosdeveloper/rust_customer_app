c8b615a7a0363371cc802081fe1cbcb466569963
abraaosantosdev <abraaofilipi12@gmail.com>
2026-07-28 11:02:37 -0300

    Implemented data verification (cannot be empty)

--- src/interface/input.rs
+++ src/interface/input.rs
@@ -7,7 +7,15 @@ pub fn input_data() -> String {
 }
 
 pub fn input_data_int() -> i32 {
-    let mut data: String = String::new();
-    io::stdin().read_line(&mut data).expect("Input failure...");
-    data.trim().parse().expect("Conversion error...")
+    loop {
+        let mut data: String = String::new();
+        io::stdin().read_line(&mut data).expect("Input failure...");
+
+        match data.trim().parse() {
+            Ok(value) => return value,
+            Err(_) => {
+                println!("Please type a valid number.");
+            }
+        }
+    }
 }
